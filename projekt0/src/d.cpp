37197c2bdb573d32670efe87efff2f86629e55a6
aasdasd <dawid-zaniewski@o2.pl>
2018-03-07 16:22:08 -0800

    pierwsza

---
+++ projekt0/src/d.cpp
@@ -0,0 +1,17 @@
+// Dawid Zaniewski
+// nr indeksu: 271286
+// prowadz¹cy: Wiktor Kuœmirek
+// nr projektu: 0
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    double a, b;
+    cout<<"podaj liczbe"<<endl;
+    cin>>a;
+    cout<<"podaj nastepna"<<endl;
+    cin>>b;
+    cout<<"wynik dodawania: "<<a+b<<endl;
+    }
